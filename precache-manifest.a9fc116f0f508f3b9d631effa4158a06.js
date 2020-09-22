12760e8b07abd4450257c922571675419d3ad430
Bogdan7 <bogdanSemenuck@gmail.com>
2020-09-22 15:33:32 +0300

    Updates

---
+++ precache-manifest.a9fc116f0f508f3b9d631effa4158a06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab645f7f67e3cf91fdcf3462ca50487",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "7aa870d50cd13c2ab983",
+    "url": "/memory/static/css/main.592104c3.chunk.css"
+  },
+  {
+    "revision": "a6c58adc9c76295eb7af",
+    "url": "/memory/static/js/2.ed7dc988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory/static/js/2.ed7dc988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa870d50cd13c2ab983",
+    "url": "/memory/static/js/main.162bd4e7.chunk.js"
+  },
+  {
+    "revision": "62e0eae13c9afaa66ed5",
+    "url": "/memory/static/js/runtime-main.c4984c0b.js"
+  }
+]);
